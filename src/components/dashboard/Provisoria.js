197e4444993658c4275aebe6900af55d389e9f77
Douglas Lundy <douglaslundy@gmail.com>
2022-05-01 17:59:14 -0300

    insert news tag in provisional page

--- src/components/dashboard/Provisoria.js
+++ src/components/dashboard/Provisoria.js
@@ -4,9 +4,11 @@ import BaseCard from "../baseCard/BaseCard";
 const Provisoria = () => {
   return (
     <BaseCard title="SysVendas">
-      <h1>Ola, seja bem vindo</h1>
-      <h4>Esta tela é provisoria, em breve teremos muitas informações siguinificativas aqui...</h4>
-      <h4>See you later...</h4>
+      <h1>Novidades...</h1>
+      <h4>#Criação de filtro por categoria na pagina produtos</h4>
+      <h6>... 01/05/2022</h6>
+      <h4>#Criação da tela preço / Categoria - que atualiza o preço de todos produtos da categoria selecionada</h4>
+      <h6>...01/05/2022</h6>
     </BaseCard>
   );
 };
