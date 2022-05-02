c814de120a8739d0e842dfbbf969fb6cda6591dc
Douglas Lundy <douglaslundy@gmail.com>
2022-05-01 21:55:43 -0300

    insert news

--- src/components/dashboard/Provisoria.js
+++ src/components/dashboard/Provisoria.js
@@ -5,9 +5,11 @@ const Provisoria = () => {
   return (
     <BaseCard title="SysVendas">
       <h1>Novidades...</h1>
-      <h4>#Criação de filtro por categoria na pagina produtos</h4>
+      <h4>#Importado a tabela de Clientes</h4>
       <h6>... 01/05/2022</h6>
-      <h4>#Criação da tela preço / Categoria - que atualiza o preço de todos produtos da categoria selecionada</h4>
+      <h4>#Desenvolvido o filtro por categoria na pagina produtos</h4>
+      <h6>... 01/05/2022</h6>
+      <h4>#Desenvolvido a tela preço / Categoria - que atualiza o preço de todos produtos da categoria selecionada</h4>
       <h6>...01/05/2022</h6>
     </BaseCard>
   );
