e3505fb14c1f93657f7caeb77e6803439fc91cd0
Douglas Lundy <douglaslundy@gmail.com>
2022-10-09 21:05:53 -0300

    added information about report on the main page

--- src/components/dashboard/Provisoria.js
+++ src/components/dashboard/Provisoria.js
@@ -5,6 +5,8 @@ const Provisoria = () => {
   return (
     <BaseCard title="SysVendas">
       <h1>Novidades...</h1>
+      <h2>#Redimensionado o tamanho do relatório de vendas para impressão!</h2>
+      <h4>... 09/10/2022</h4>     
       <h4>#Criado função de imprimir relatorio de vendas</h4>
       <h6>... 27/07/2022</h6>
       <h4>#Criado função de recebimento de debitos por cliente</h4>
@@ -15,8 +17,6 @@ const Provisoria = () => {
       <h6>... 01/06/2022</h6>
       <h4>#Criado Módulo de PDV</h4>
       <h6>... 25/05/2022</h6>
-      <h4>#Criado Função de controle de estoque integrado</h4>
-      <h6>... 20/05/2022</h6>     
     </BaseCard>
   );
 };
