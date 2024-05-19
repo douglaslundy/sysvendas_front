3493608cf17a82af9654f28876e50d2d3a927aec
Douglas Lundy <douglaslundy@gmail.com>
2024-05-19 12:53:58 -0300

    altered convert2decimal rule

--- src/components/helpers/formatt/currency/index.js
+++ src/components/helpers/formatt/currency/index.js
@@ -13,21 +13,27 @@ export function convertToDecimalWith3DigitsAfterComma(str = 0) {
 }
 
 export function convertToDecimalWith2DigitsAfterComma(str = 0) {
-    const number = str?.toString().replace(',','.');    
-
-    let dotIndex = number?.indexOf('.');
-    var result = dotIndex > 0 ? number?.slice(0, dotIndex + 3) : number;
-
-    return parseFloat(result).toFixed(2);
+    const number = parseFloat(str?.toString().replace(',','.'));     
+    return number.toFixed(2);
 }
+
+// export function convertToDecimalWith2DigitsAfterComma(str = 0) {
+//     const number = str?.toString().replace(',','.');    
+
+//     let dotIndex = number?.indexOf('.');
+//     var result = dotIndex > 0 ? number?.slice(0, dotIndex + 3) : number;
+
+//     return parseFloat(result).toFixed(2);
+// }
 
 export function setCurrency(str = '') {
     //remove todos os pontos
-    let multiplier = str?.toString().replace(/['R$']/g, '');
+    let value = str?.toString().replace(/['R$']/g, '');
     
     // troca a vergula pelo ponto, afim de transformar em numero monetário
-    multiplier = convertToDecimalWith2DigitsAfterComma(multiplier?.replace(',', '.'));
-    return parseFloat(multiplier?.trim()).toFixed(2);
+    // multiplier = convertToDecimalWith2DigitsAfterComma(multiplier?.replace(',', '.'));
+    // multiplier = convertToDecimalWith2DigitsAfterComma(multiplier);
+    return convertToDecimalWith2DigitsAfterComma(value?.trim());
 }
 
 export function changeDotToComma(str) {
