b30e6b4197107c928a2f71ed8398182e70a5eadf
Douglas Lundy <douglaslundy@gmail.com>
2024-07-01 18:10:09 -0300

    altered backend url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { parseCookies } from 'nookies';
 const { 'sysvendas.token': token } = parseCookies();
 
 export const api = axios.create({
-    baseURL: 'https://sysvendas.douglaslundy.com.br/api'
+    baseURL: 'https://sysvendas.dlsistemas.com.br/api'
     // baseURL: 'http://localhost:8000/api'
 });
 
